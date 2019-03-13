cd4df063b131587d6d8e92e835270b648b2f2a41
geekseong <geekseong@gmail.com>
2019-03-13 10:26:54 +0900

    Add REST Controller & DTO

---
+++ Jpa/src/main/java/com/jpa/Domain/Posts/PostsSaveRequestDto.java
@@ -0,0 +1,36 @@
+package com.jpa.Domain.Posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+
+    Controller 에서 @RequestBody 로 외부에서 데이터를 받는 경우에는 기본생성자 + set 메소드를 통해서만 값이 할당됨.
+    그래서 이때만 setter를 허용
+
+    테이블과 매핑되는 Entity 클래스를 Request/Response 클래스로 사용해서는 안됨.
+    Entity 클래스는 가장 Core 한 클래스.
+    수많은 서비스 클래스나 비지니스 로직들이 Entity 클래스를 기준으로 동작.
+    Entity 클래스가 변경되면 여러 클래스에 영향을 끼치게 되는 반명 Request 와 Response 용 DTO 는 View 를 위한 클래스라 정말 자주 변경이 필요.
+
+    따라서 View Layer 와 DB Layer 를 철저하게 역할 분리하는게 좋음.
+
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String contents;
+    private  String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .contents(contents)
+                .title(title)
+                .author(author)
+                .build();
+    }
+}
---
+++ Jpa/src/main/java/com/jpa/Domain/Posts/WebRestController.java
@@ -0,0 +1,38 @@
+package com.jpa.Domain.Posts;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+     스프링 프레임워크에서 객체를 주입 받는 방법은3가지
+
+        - @autowired
+        - setter
+        - 생성자
+
+        이중 가장 권장하느 방법이 '생성자로 주입받는 방식' 입니다( @Autowired는 비 권장방식)
+
+        @AllArgsConstructor 는 모든 필드를 인자값으로 하는 생서자를 Lombok에서 자동으로 대신 생성.
+        의존성 관계가 변경될떄마다 생성자 코드를 계속해서 수정하는 번거로움을 해결.
+
+
+ */
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+}
