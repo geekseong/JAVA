2e4b6b93530390cfea7c3c24d6a84e519f527f76
성락원 <rakwonseong@seonglag-won-ui-MacBook-Pro.local>
2019-03-13 23:26:55 +0900

    Modify WebRestController field PostsRespository -> PostsService

--- Jpa/src/main/java/com/jpa/Webservice/Web/WebRestController.java
+++ Jpa/src/main/java/com/jpa/Webservice/Web/WebRestController.java
@@ -2,6 +2,7 @@ package com.jpa.Webservice.Web;
 
 import com.jpa.Webservice.Domain.Posts.PostsRepository;
 import com.jpa.Webservice.Dto.Posts.PostsSaveRequestDto;
+import com.jpa.Webservice.Service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello(){
@@ -35,6 +36,6 @@ public class WebRestController {
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+        postsService.save(dto);
     }
 }
